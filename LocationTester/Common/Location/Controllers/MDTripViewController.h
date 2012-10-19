228369f306942844c2db6b3a2457b4d7bce17c85
Pawan Poudel <pawan.poudel@gmail.com>
2012-10-18 23:09:04 -0400

    Create trip view controller that allows users to record trips

---
+++ LocationTester/Common/Location/Controllers/MDTripViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDTripViewController.h
+//  LocationTester
+//
+//  Created by PAWAN POUDEL on 10/18/12.
+//  Copyright (c) 2012 Mobile Defense Inc. All rights reserved.
+//
+
+#import "MDLocationManager.h"
+
+@interface MDTripViewController : UIViewController <MDLocationManagerDelegate>
+
+@end
