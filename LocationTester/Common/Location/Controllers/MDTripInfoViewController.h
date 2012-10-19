b21843715864f79b9f37fedef89caddab6cc11e5
Pawan Poudel <pawan.poudel@gmail.com>
2012-10-19 03:13:21 -0400

    Add trip info view controller
    This view will eventually display all trip data including
    a mapview that displays start and end street addresses.

---
+++ LocationTester/Common/Location/Controllers/MDTripInfoViewController.h
@@ -0,0 +1,15 @@
+//
+//  MDTripInfoViewController.h
+//  LocationTester
+//
+//  Created by PAWAN POUDEL on 10/19/12.
+//  Copyright (c) 2012 Mobile Defense Inc. All rights reserved.
+//
+
+@class MDTrip;
+
+@interface MDTripInfoViewController : UIViewController
+
+@property (strong, nonatomic) MDTrip *trip;
+
+@end
